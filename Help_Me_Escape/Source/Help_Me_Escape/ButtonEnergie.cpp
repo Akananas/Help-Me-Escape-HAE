d86be2dc3fa8858162cb03d0b5cbee794978d696
François Charrier <francois.charrier23@gmail.com>
2021-03-09 15:44:36 +0100

    Bouton pour l'enigme de l'energie

---
+++ Help_Me_Escape/Source/Help_Me_Escape/ButtonEnergie.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ButtonEnergie.h"
+#include "Components/ButtonSlot.h"
+
+/*UButtonEnergie::UButtonEnergie()
+{
+    OnClicked.AddDynamic(this, &UButtonEnergie::OnClick);
+
+    //Bind function
+    load.AddDynamic(this, &UButtonEnergie::LoadGameFile);
+}
+
+void UButtonEnergie::OnClick()
+{
+    load.Broadcast(this->GetName());
+}
+
+void UButtonEnergie::LoadGameFile(FString name)
+{
+   
+}
+
+void UButtonEnergie::SetVal(const uint8& _index) {
+    index = _index;
+}*/
+
---
+++ Help_Me_Escape/Source/Help_Me_Escape/ButtonEnergie.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Button.h"
+#include "Delegates/Delegate.h"
+#include "ButtonEnergie.generated.h"
+/**
+ * 
+ */
+/*DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FLoadDelegate, FString, name);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FClickDelegate);*/
+UCLASS()
+class HELP_ME_ESCAPE_API UButtonEnergie : public UButton
+{
+	GENERATED_BODY()
+
+public:
+    UButtonEnergie();
+
+   /* UPROPERTY()
+        FLoadDelegate load;
+
+    UPROPERTY()
+        FClickDelegate click;
+    UFUNCTION()
+        void OnClick();
+    UFUNCTION()
+        void LoadGameFile(FString name);
+    UFUNCTION(BlueprintCallable, Category = "Utilities")
+        void SetVal(const uint8& _index);
+    TArray<bool>* arrayPtr = nullptr;
+    */
+protected:
+    
+    //uint8 index = 0;
+
+};
